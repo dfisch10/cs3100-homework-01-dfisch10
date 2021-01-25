7311ed1c0a4a9aa191d388b4d752a0d5d46f8bc4
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2021-01-25 10:44:25 -0700

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 int
 main(int argc, char *argv[])
 {
+    time_t currentTime;
+    time(&currentTime);
+
+    struct tm *myTime = localtime(&currentTime);
+        int month = myTime->tm_mon + 1;
+        int year = myTime->tm_year + 1900;
+
+    printf("The current date and time is: %i/%i/%i %i:%i:%i\n", month, myTime->tm_mday, year, myTime->tm_hour, myTime->tm_min, myTime->tm_sec);
+
     printf("hello world (pid:%d)\n", (int) getpid());
     int rc = fork();
     if (rc < 0) {
@@ -19,5 +29,6 @@ main(int argc, char *argv[])
         printf("hello, I am parent of %d (pid:%d)\n",
 	       rc, (int) getpid());
     }
+
     return 0;
 }
